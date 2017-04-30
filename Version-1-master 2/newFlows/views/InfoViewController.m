57d4a0461c0f5febc3e24b25771f5595c200eb4b
Josh <joshua.woods@possible.com>
2017-04-30 14:35:02 -0600

    Info View Update

--- Version-1-master 2/newFlows/views/InfoViewController.m
+++ Version-1-master 2/newFlows/views/InfoViewController.m
@@ -5,21 +5,17 @@
 //
 
 #import "InfoViewController.h"
-//#import "GDIIndexBar.h"
-//#import "AddDetailViewController.h"
 #import "UIColor+Hexadecimal.h"
-//#import "iRate.h"
 #import <MessageUI/MessageUI.h>
 #import <QuartzCore/QuartzCore.h>
 #import "infoCell.h"
 
 
-@interface InfoViewController () <MFMailComposeViewControllerDelegate, UITableViewDelegate, UITableViewDataSource >
+@interface InfoViewController () <MFMailComposeViewControllerDelegate>
 
-@property (weak, nonatomic) IBOutlet UITableView *mainTable;
-@property (weak, nonatomic) IBOutlet UIView *highView;
-@property (weak, nonatomic) IBOutlet UIView *normalView;
-@property (weak, nonatomic) IBOutlet UIView *lowView;
+@property (weak, nonatomic) IBOutlet UIButton *feedbackButton;
+@property (weak, nonatomic) IBOutlet UIView *feedbackSeparator;
+@property (weak, nonatomic) IBOutlet UIButton *disclaimerButton;
 
 @end
 
@@ -30,111 +26,40 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
-                                                  forBarMetrics:UIBarMetricsDefault];
-    self.navigationController.navigationBar.shadowImage = [UIImage new];
-    self.navigationController.navigationBar.translucent = YES;
-    
-    UIImageView* img = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"HeaderLogo"]];
-    self.navigationItem.titleView = img;
-    
-    [self.mainTable setSeparatorColor:[UIColor colorWithRed:0.20 green:0.20 blue:0.20 alpha:1.0]];
-    
-    _highView.layer.cornerRadius = 5;
-    [_highView setBackgroundColor:[UIColor colorWithRed:0.15 green:0.58 blue:1.00 alpha:1.0]];
-    _normalView.layer.cornerRadius = 5;
-    [_normalView setBackgroundColor:[UIColor colorWithRed:0.42 green:0.91 blue:0.46 alpha:1.0]];
-    _lowView.layer.cornerRadius = 5;
-    [_lowView setBackgroundColor:[UIColor colorWithRed:0.93 green:0.39 blue:0.25 alpha:1.0]];
+    [self applyDesign];
 }
 
 - (IBAction)backClicked:(id)sender{
     
     [self.navigationController popViewControllerAnimated:NO];
-    NSLog(@"back clicked");
 }
 
-#pragma mark - UITableviewDelegate
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return 2;
-}
-
--(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(nonnull NSIndexPath *)indexPath
-{
-    return 66.0f;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (void)applyDesign {
+    [self.navigationController.navigationBar setBackgroundImage:[UIImage new]
+                                                  forBarMetrics:UIBarMetricsDefault];
+    self.navigationController.navigationBar.shadowImage = [UIImage new];
+    self.navigationController.navigationBar.translucent = YES;
     
-    static NSString *CellIdentifier = @"infoCell";
-    infoCell *cell = [_mainTable dequeueReusableCellWithIdentifier:CellIdentifier];
-    cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    cell.mainLabel.textAlignment = NSTextAlignmentCenter;
+    self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"HeaderLogo"]];
     
-    NSString *titleString;
+    UIFont *font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:18.0f];
+    UIColor *color = [UIColor colorWithHex:@"ACACAC"];
+    [self.feedbackButton.titleLabel setFont:font];
+    [self.feedbackButton setTitleColor:color forState:UIControlStateNormal];
+    [self.feedbackButton.titleLabel setText:@"Send Feedback"];
+    [self.disclaimerButton.titleLabel setFont:font];
+    [self.disclaimerButton setTitleColor:color forState:UIControlStateNormal];
+    [self.disclaimerButton.titleLabel setText:@"Disclaimer & Restore"];
     
-    if (indexPath.row == 0) {
-        titleString = @"Send Feedback";
-    }else{
-        titleString = @"Disclaimer & Restore";
-    }
-    
-    NSDictionary *attributes = @{NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-Thin" size:18.0f],
-                                 NSForegroundColorAttributeName: [UIColor colorWithHex:@"ACACAC"]};//[UIColor colorWithHex:@"ACACAC"]};
-    NSAttributedString *cellString = [[NSAttributedString alloc] initWithString:titleString attributes:attributes];
-    
-    cell.mainLabel.attributedText = cellString;
-    
-    return cell;
-    
-}
-
--(void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    [cell setSeparatorInset:UIEdgeInsetsZero];
-    [cell setPreservesSuperviewLayoutMargins:NO];
-    [cell setLayoutMargins:UIEdgeInsetsZero];
-}
-
--(CGFloat)tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
-{
-    return 0.5f;
-}
-
-
-- (void)tableView:(UITableView *)tableView willDisplayHeaderView:(UIView *)view forSection:(NSInteger)section {
-    
-    view.tintColor = [UIColor colorWithRed:0.20 green:0.20 blue:0.20 alpha:1.0];
-    
-    
-}
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (indexPath.row == 0) {
-        [self feedbackClicked];
-    }else{
-        [self performSegueWithIdentifier:@"disclaimerSegue" sender:self];
+    if ([MFMailComposeViewController canSendMail] == NO) {
+        self.feedbackButton.hidden = YES;
+        self.feedbackSeparator.hidden = YES;
     }
 }
-
-
 
 #pragma mark - IBActions
 
-- (IBAction)disclaimerClicked:(id)sender {
-    [self performSegueWithIdentifier:@"disclaimerSegue" sender:self];
-}
-
-- (void)feedbackClicked {
+- (IBAction)feedbackTapped:(id)sender {
     if ([MFMailComposeViewController canSendMail])
     {
         MFMailComposeViewController *mailer = [[MFMailComposeViewController alloc] init];
@@ -151,6 +76,7 @@
         [self presentViewController:mailer animated:YES completion:nil];
         
     } else {
+        // This should never be reached as we are hiding the button if there is no capability of sending an email. However, we will leave it here as a fallback.
         UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Error" message:@"It seems as though your device is unable to send emails." preferredStyle:UIAlertControllerStyleAlert];
         UIAlertAction *okay = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
         [alert addAction:okay];
@@ -158,6 +84,9 @@
     }
 }
 
+- (IBAction)disclaimerTapped:(id)sender {
+    [self performSegueWithIdentifier:@"disclaimerSegue" sender:self];
+}
 
 - (void)mailComposeController:(MFMailComposeViewController*)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError*)error
 {
