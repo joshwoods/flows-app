7ff9efbcf582af4d08e39ab569aa60401fbcbd68
Josh <joshua.woods@possible.com>
2017-04-30 18:14:46 -0600

    fully removing empty data set from PurchaseViewController
    this was entirely unneeded, it was MORE work to have this in there than
    not…🤷🏻‍♂️

--- Version-1-master 2/newFlows/views/PurchaseViewController.m
+++ Version-1-master 2/newFlows/views/PurchaseViewController.m
@@ -91,56 +91,7 @@
     [self.navigationController popViewControllerAnimated:NO];
 }
 
-#pragma mark - EmptyDataset
-
-- (NSAttributedString *)buttonTitleForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state
-{
-    UIFont *font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:17.0f];
-    UIColor *textColor = [UIColor colorWithHex:(state == UIControlStateNormal) ? @"ffffff" : @"acacac"];
-    NSMutableDictionary *attributes = [NSMutableDictionary new];
-    if (font) [attributes setObject:font forKey:NSFontAttributeName];
-    if (textColor) [attributes setObject:textColor forKey:NSForegroundColorAttributeName];
-    
-    return [[NSAttributedString alloc] initWithString:@"Upgrade" attributes:attributes];
-}
-
-- (UIImage *)buttonBackgroundImageForEmptyDataSet:(UIScrollView *)scrollView forState:(UIControlState)state{
-    NSString *imageName = @"addStation.png";
-    return [[[UIImage imageNamed:imageName] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0) resizingMode:UIImageResizingModeStretch] imageWithAlignmentRectInsets:UIEdgeInsetsMake(0, -self.view.bounds.size.width/4, 0, -self.view.bounds.size.width/4)];
-}
-
-- (CGFloat)spaceHeightForEmptyDataSet:(UIScrollView *)scrollView
-{
-    return self.view.bounds.size.height/8;
-}
-
-- (CGFloat)verticalOffsetForEmptyDataSet:(UIScrollView *)scrollView
-{
-    return -65.0;
-}
-
-#pragma mark - DZNEmptyDataSetDelegate Methods
-
-- (BOOL)emptyDataSetShouldDisplay:(UIScrollView *)scrollView
-{
-    return YES;
-}
-
-#pragma mark - UITableviewDelegate
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-    return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    return 0;
-}
-
-#pragma mark -
-
-- (void)emptyDataSetDidTapButton:(UIScrollView *)scrollView
+- (IBAction)upgradeTapped:(id)sender
 {
     [[MKStoreKit sharedKit] initiatePaymentRequestForProductWithIdentifier:@"com.flowsapp.flowspro"];
 }
